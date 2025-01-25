fa141556153c4fef1db9885347b4affd3ba24c20
Atanu Pandit <atanupandit623@gmail.com>
2025-01-26 02:26:45 +0530

    edit button restyle

--- Frontend/src/components/BlogItem.jsx
+++ Frontend/src/components/BlogItem.jsx
@@ -53,7 +53,7 @@ const BlogItem = ({ blog, onDelete, onEdit }) => {
 
       {/* Buttons */}
 
-      <div className="flex place-content-between mt-2 px-6 pb-4">
+      <div className="flex place-content-evenly md:place-content-between mt-2 px-6 pb-4">
         <button  onClick={handleReadMore} className="bg-blue-500 text-white px-4 py-2 rounded">
           Read More
         </button>
