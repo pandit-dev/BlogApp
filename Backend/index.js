9df2af6d7b51239cbc19bc4249aeca59d6422e99
Atanu Pandit <atanupandit623@gmail.com>
2025-01-26 02:03:13 +0530

    production url changed

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://blogapp-krlf.onrender.com", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://blogapp-krlf.onrender.com',
   withCredentials: true
 });
 
