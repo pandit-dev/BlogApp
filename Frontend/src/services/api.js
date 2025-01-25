4ff365afe419f3cdeba6059b51ca8a9702ea0a62
Atanu Pandit <atanupandit623@gmail.com>
2025-01-26 02:07:52 +0530

    production url changed

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://blogapp-krlf.onrender.com',
+  baseURL: 'https://blogapp-krlf.onrender.com/api/',
   withCredentials: true
 });
 
