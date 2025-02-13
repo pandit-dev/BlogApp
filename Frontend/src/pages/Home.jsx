52220508d916994741709e4efa2d825f48fa760e
Atanu Pandit <atanupandit623@gmail.com>
2025-02-13 17:34:25 +0530

    Home route changed

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <>
     <div className="relative bg-cover bg-center min-h-screen" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D')" }}>
-      <div className="bg-black bg-opacity-50 h-full flex flex-col justify-center items-center text-center text-white px-4">
+      <div className="bg-black bg-opacity-50 min-h-full flex flex-col justify-center items-center text-center text-white px-4">
         <h1 className="text-5xl font-extrabold mb-4">Welcome to Our Blog</h1>
         <p className="text-lg mb-8">Discover the latest insights, stories, and trends in our dynamic blogs.</p>
         <a href="/blog" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">
